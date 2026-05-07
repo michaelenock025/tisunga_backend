ebc724f6f6a28c869f85c173bfe21df0d78b8c64
michaelenock025 <michaelenock025@gmail.com>
2026-05-07 10:11:47 +0200

    sms sandbox fixes

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ const notificationRoutes = require('./routes/notification.routes');
 const webhookRoutes      = require('./routes/webhook.routes');
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(helmet());
 app.use(cors({
